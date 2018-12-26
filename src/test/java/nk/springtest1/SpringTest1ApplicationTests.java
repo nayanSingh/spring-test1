1623b965d992252e31a570e4066ce7c5556ffd01
nayan196 <nayansingh196@gmail.com>
2018-12-27 00:02:19 +0530

    ok

---
+++ src/test/java/nk/springtest1/SpringTest1ApplicationTests.java
@@ -0,0 +1,17 @@
+package nk.springtest1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringTest1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
